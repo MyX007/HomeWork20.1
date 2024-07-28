128e71dc849d967d4ea4dee51096619559560e4e
Vladimir Blyntsev <blyncev@yandex.ru>
2024-07-28 19:56:18 +0700

    The project has been finalized

--- main/management/commands/fill.py
+++ main/management/commands/fill.py
@@ -33,14 +33,19 @@ class Command(BaseCommand):
 
         for category in Command.json_read_categories():
             category_for_create.append(
-                Category(**category['fields'])
+                Category(name=category['fields']['name'], description=category['fields']['description'])
             )
 
         Category.objects.bulk_create(category_for_create)
 
         for product in Command.json_read_products():
             product_for_create.append(
-                Product(**product['fields'])
+                Product(name=product['fields']['name'],
+                        description=product['fields']['description'],
+                        category=Category.objects.get(pk=product['fields']['category']),
+                        img=product['fields']['img'],
+                        price=product['fields']['price'], created_at=product['fields']['created_at'],
+                        updated_at=product['fields']['updated_at'])
             )
 
         Product.objects.bulk_create(product_for_create)
