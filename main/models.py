f8e0a140b57f2aa547b6803afd94f240ab8f40f7
Vladimir Blyntsev <blyncev@yandex.ru>
2024-07-31 21:29:13 +0700

    The project has been finalized

--- main/models.py
+++ main/models.py
@@ -18,12 +18,12 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название')
-    description = models.CharField(max_length=150, verbose_name='Описание')
+    description = models.TextField(max_length=150, verbose_name='Описание')
     img = models.ImageField(upload_to='products_img/', verbose_name='Фото', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Категория')
-    price = models.IntegerField(verbose_name="Цена", **NULLABLE)
-    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now=True)
-    updated_at = models.DateTimeField(verbose_name="Дата последнего изменения", auto_now_add=True)
+    price = models.PositiveIntegerField(verbose_name="Цена")
+    created_at = models.DateTimeField(verbose_name="Дата создания", add_auto_now=True)
+    updated_at = models.DateTimeField(verbose_name="Дата последнего изменения", now_add=True)
 
     def __str__(self):
         return f"{self.name} ({self.category}) - {self.price}"
