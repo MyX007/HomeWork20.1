408bac0eeaf60ad01c6c398fd9d2574ab0568fb4
Vladimir Blyntsev <blyncev@yandex.ru>
2024-08-01 21:02:53 +0700

    The project has been finalized

--- main/models.py
+++ main/models.py
@@ -22,8 +22,8 @@ class Product(models.Model):
     img = models.ImageField(upload_to='products_img/', verbose_name='Фото', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Категория')
     price = models.PositiveIntegerField(verbose_name="Цена")
-    created_at = models.DateTimeField(verbose_name="Дата создания", add_auto_now=True)
-    updated_at = models.DateTimeField(verbose_name="Дата последнего изменения", now_add=True)
+    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Дата последнего изменения", auto_now=True)
 
     def __str__(self):
         return f"{self.name} ({self.category}) - {self.price}"
